64e7e030242b11631d8691d111435bec4194d44e
akasfei <akasfei@gmail.com>
2013-07-01 01:06:51 +0800

    functional program

--- base64src/base64.asm
+++ base64src/base64.asm
@@ -1,13 +1,30 @@
-toBASE64 PROC pSrc:DWORD, pOut:DWORD, len:DWORD, pOutSize:DWORD
+CONV MACRO ;convert base64 character to base256, input char in DL, output DL
+    .IF DL == '+'
+        MOV DL, 62
+    .ELSEIF DL == '/'
+        MOV DL, 63
+    .ELSEIF DL >= '0' && DL <= '9'
+        ADD DL, 4
+    .ELSEIF DL >= 'a' && DL <= 'z'
+        SUB DL, 71
+    .ELSEIF DL >= 'A' && DL <= 'Z'
+        SUB DL, 65
+    .ENDIF
+ENDM
+
+
+toBASE64 PROC pSrc:DWORD, pOut:DWORD, len:DWORD
 
     MOV ECX, len
     MOV ESI, pSrc ;bytes from source
-    MOV EDI, OFFSET base64tab
+    LEA EDI, base64tab
     PUSH EBP
     MOV EBP, pOut
-    MOV DWORD PTR [pOutSize], 0
+    LEA EAX, writeSize
+    MOV DWORD PTR [EAX], 0
 src_byteLoop:    
-    ADD DWORD PTR [pOutSize], 4
+    LEA EAX, writeSize
+    ADD DWORD PTR [EAX], 4
 
     XOR EAX, EAX
 
@@ -61,7 +78,8 @@ src_byteLoop:
 
     JZ finished
         ;need to pad out some extra bytes
-        ADD DWORD PTR [pOutSize], 4
+        LEA EAX, writeSize
+        ADD DWORD PTR [EAX], 4
 
         XOR EAX, EAX
         ;read in 3 bytes regardless of junk data following pSrc - already zero from above)
@@ -105,27 +123,27 @@ src_byteLoop:
         SHL EAX, 6 ;done first 6 bits                        
 
         SHR EDX, 26
-        MOV BL, BYTE PTR [EDI+EDX]             ;put char in buffer
+        MOV BL, BYTE PTR [EDI+EDX] ;put char in buffer
         MOV BYTE PTR[EBP], BL
-        INC EBP                                           ;next buf
+        INC EBP ;next buf
 
          ;manipulate in EDX byte 3
         MOV EDX, EAX
-        SHL EAX, 6                                     ;done first 6 bits                        
+        SHL EAX, 6 ;done first 6 bits                        
 
         SHR EDX, 26
-        MOV BL, BYTE PTR [EDI+EDX]                   ;put char in buffer
+        MOV BL, BYTE PTR [EDI+EDX] ;put char in buffer
         MOV BYTE PTR[EBP], BL
-        INC EBP                                           ;next buf
+        INC EBP ;next buf
 
-        MOV EAX, ECX                               ;'return' pad count
+        MOV EAX, ECX ;'return' pad count
 
 finished:
         TEST EAX, EAX
         JZ endF
         ;some bytes were padding, put them as =
 
-        SUB EBP, EAX  ;move ptr back for num bytes to pad
+        SUB EBP, EAX ;move ptr back for num bytes to pad
 padChars:
         MOV BYTE PTR[EBP], '='
         INC EBP
@@ -138,3 +156,80 @@ endF:
     RET
                   
 toBASE64 ENDP
+
+toBYTE PROC pSrc:DWORD, pOut:DWORD, len:DWORD
+    PUSH EBP
+    LEA EBX, writeSize
+    MOV DWORD PTR [EBX], 0
+    MOV ECX, len
+    MOV ESI, pSrc ;bytes from source
+    PUSH EBP
+    MOV EBP, pOut
+    
+    CMP ECX, 4
+    JBE last_24b
+    
+dec_24bloop:
+    LEA EBX, writeSize
+    ADD DWORD PTR [EBX], 3
+    XOR EAX, EAX ;clear EAX
+    MOV EDI, 3
+dec_byteloop:
+    MOV DL, BYTE PTR [ESI]
+    CONV
+    ADD AL, DL
+    SHL EAX, 6
+    INC ESI
+    DEC EDI
+    JNS dec_byteloop
+    
+    SHR EAX, 6
+    MOV EDI, 2
+dec_outbyte:
+    MOV BYTE PTR[EBP+EDI], AL
+    SHR EAX, 8
+    DEC EDI
+    JNS dec_outbyte
+    
+    ADD EBP, 3
+    SUB ECX, 4
+    CMP ECX, 4
+    JA dec_24bloop
+    
+last_24b:
+    XOR EAX, EAX
+    MOV EDI, 3
+last_byteloop:
+    CMP BYTE PTR [ESI], '='
+    JE last_out
+    MOV DL, BYTE PTR [ESI]
+    CONV
+    ADD AL, DL
+    SHL EAX, 6
+    INC ESI
+    DEC EDI
+    JNS last_byteloop
+    
+last_out:
+    SHR EAX, 6
+    .IF EDI == 0 ;one '=' at the end
+        SHR EAX, 2
+        MOV EDI, 1
+        ADD DWORD PTR [EBX], 2
+    .ELSEIF EDI == 1 ; two '=' at the end
+        SHR EAX, 4
+        MOV EDI, 0
+        ADD DWORD PTR [EBX], 1
+    .ELSE
+        MOV EDI, 2
+        ADD DWORD PTR [EBX], 3
+    .ENDIF
+last_outloop:
+    MOV BYTE PTR[EBP+EDI], AL
+    SHR EAX, 8
+    DEC EDI
+    JNS last_outloop
+    
+    POP EBP
+    RET
+toBYTE ENDP 
--- base64src/main.asm
+++ base64src/main.asm
@@ -9,8 +9,8 @@ includelib \masm32\lib\kernel32.lib
 
 ;INCLUDE macros.asm
 
-toBASE64 PROTO :DWORD, :DWORD, :DWORD, :DWORD
-;toBYTE PROTO :DWORD, :DWORD, :DWORD
+toBASE64 PROTO :DWORD, :DWORD, :DWORD
+toBYTE PROTO :DWORD, :DWORD, :DWORD
 
 .DATA
     MsgBoxCaption	DB "Please select working method.",0 
@@ -20,14 +20,19 @@ toBASE64 PROTO :DWORD, :DWORD, :DWORD, :DWORD
     desFileName DB "dest.txt", 0
     
     DBNOTE1 DB "Encrypting source.txt", 0
-    DBNOTE2 DB "base64.asm line 64", 0
+    DBNOTE2 DB "Decrypting desc.txt", 0
     DBNOTE3 DB "base64.asm line 66", 0
     DBNOTE4 DB "base64.asm", 0
     DBNOTE5 DB "<= 10", 0
     
+    writeSize DD 0
+    
     base64tab DB "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 .DATA? 
+    pSFileName DWORD ?
+    pDFileName DWORD ?
+
     hSFile HANDLE ? 
     hSMemory HANDLE ? 
     pSMemory DWORD ? 
@@ -49,12 +54,7 @@ toBASE64 PROTO :DWORD, :DWORD, :DWORD, :DWORD
 .CODE 
 start:
 
-    INVOKE CreateFile, ADDR srcFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
-    MOV hSFile, EAX
-    INVOKE CreateFile, ADDR desFileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
-    MOV hDFile, EAX
-    
-    INVOKE GlobalAlloc, GMEM_MOVEABLE OR GMEM_ZEROINIT, SRCMSIZE 
+    INVOKE GlobalAlloc, GMEM_MOVEABLE OR GMEM_ZEROINIT, DESMSIZE 
     MOV hSMemory, EAX
     INVOKE GlobalLock, hSMemory 
     MOV pSMemory, EAX
@@ -66,29 +66,46 @@ start:
     INVOKE MessageBox, NULL, ADDR MsgBoxText, ADDR MsgBoxCaption, MB_YESNOCANCEL
 	
     .IF EAX == IDYES
+        INVOKE CreateFile, ADDR srcFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
+        MOV hSFile, EAX
+        INVOKE CreateFile, ADDR desFileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+        MOV hDFile, EAX
         INVOKE MessageBox, NULL, ADDR DBNOTE1, ADDR srcFileName, MB_OK
+        ;MOV pSFileName, OFFSET srcFileName
+        ;MOV pDFileName, OFFSET desFileName
         JMP READF
-	.ELSEIF EAX == IDNO
+    .ELSEIF EAX == IDNO
+        INVOKE CreateFile, ADDR desFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
+        MOV hSFile, EAX
+        INVOKE CreateFile, ADDR srcFileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+        MOV hDFile, EAX
+        INVOKE MessageBox, NULL, ADDR DBNOTE2, ADDR desFileName, MB_OK
+        ;MOV pSFileName, OFFSET desFileName
+        ;MOV pDFileName, OFFSET srcFileName
         JMP DECRYPT
-	.ELSEIF EAX == IDCANCEL
+    .ELSEIF EAX == IDCANCEL
         JMP MEXIT
-	.ENDIF
+    .ENDIF
+    
+    
     
 READF:
     INVOKE ReadFile, hSFile, pSMemory, SRCMSIZE, ADDR SReadSize, NULL
-    INVOKE toBASE64, pSMemory, pDMemory, SReadSize, ADDR DWriteSize
-    MOV EAX, SReadSize
-    MOV EDX, 0
-    MOV ECX, 3
-    DIV ECX
-    .IF EDX > 0
-        ADD EAX, 1
-    .ENDIF
-    MOV ECX, 4
-    MUL ECX
-    MOV DWriteSize, EAX
+    INVOKE toBASE64, pSMemory, pDMemory, SReadSize
+    ;MOV EAX, SReadSize
+    ;MOV EDX, 0
+    ;MOV ECX, 3
+    ;DIV ECX
+    ;.IF EDX > 0
+    ;    ADD EAX, 1
+    ;.ENDIF
+    ;MOV ECX, 4
+    ;MUL ECX
+    ;MOV DWriteSize, EAX
     ;INVOKE MessageBox, NULL, pDMemory, ADDR srcFileName, MB_OK
-    INVOKE WriteFile, hDFile, pDMemory, DWriteSize, ADDR writeStats, NULL
+    MOV EAX, writeSize
+    MOV DWriteSize, EAX
+    INVOKE WriteFile, hDFile, pDMemory, writeSize, ADDR writeStats, NULL
         
     ;.IF SReadSize <= SRCMSIZE
     ;    INVOKE MessageBox, NULL, pSMemory, ADDR srcFileName, MB_OK
@@ -97,7 +114,18 @@ READF:
     ;.ENDIF
     JMP MEXIT
 DECRYPT:
-
+    INVOKE ReadFile, hSFile, pSMemory, DESMSIZE, ADDR SReadSize, NULL
+    INVOKE toBYTE, pSMemory, pDMemory, SReadSize
+    ;MOV EAX, SReadSize
+    ;MOV EDX, 0
+    ;MOV ECX, 4
+    ;DIV ECX
+    ;MOV ECX, 3
+    ;MUL ECX
+    ;MOV DWriteSize, EAX
+    ;INVOKE MessageBox, NULL, pDMemory, ADDR srcFileName, MB_OK
+    INVOKE WriteFile, hDFile, pDMemory, writeSize, ADDR writeStats, NULL
+    JMP MEXIT
     
     
 MEXIT:
@@ -108,7 +136,7 @@ MEXIT:
     INVOKE GlobalFree, hDMemory 
     INVOKE CloseHandle, hDFile 
 
-	INVOKE ExitProcess,NULL
-    
+    INVOKE ExitProcess,NULL
+      
     INCLUDE base64.asm
 END start
